76ea0fffd95091b983e06757ea8e0ed34abd3f68
dpriyankaprdxn <priyankad@axioned.com>
2022-02-18 21:13:11 +0530

    load more button added

--- wp-content/themes/cardekho/Cars.php
+++ wp-content/themes/cardekho/Cars.php
@@ -39,16 +39,27 @@ if (count($categories_name) > 0 ) {
 $arguments= [
   'post_type' => 'Car',
   'posts_per_page' => -1,
-  'post_status' => 'publish'
+  'post_status' => 'publish',
+  'orderby' => 'ID',
+  'order' => 'ASC'
 ];
 
 $cars = new WP_Query($arguments);
-if($cars->have_posts()) {  ?>
+if($cars->have_posts()) {  
+  $ids = array_column($cars->posts, 'ID');
+  $last_id = end($ids);
+?>
   <ul class="car-tab-container cars">
     <?php
       include('template-parts/car.php');
     ?>
   </ul>
 <?php } ?>
+<?php if ($cars->max_num_pages >1) { ?>
+  <div class="button_load_more ">
+    <a  class="loadmore "href="#FIXME">Load More</a>
+  </div>
+<?php  }?>
+
 <h3>Search Result</h3>
 <ul class="car-tab cars"></ul>
